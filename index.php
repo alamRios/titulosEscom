85ec35fa4d059e4bc1ac8d50e08f94563c946f43
alamRios <alam.rios@ymail.com>
2017-11-21 19:31:05 -0600

    Estructura

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ESCOM - TITULOS</title>
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js" />
+    <script type="text/javascript" src="js/materialize.min.js"/>
+  </head>
+  <body>
+    
+  </body>
+</html>
