e06769d2a113bdec256115f3982c65e9c8de5756
Floranyelli Anaya <33078992+FloranyelliA@users.noreply.github.com>
2017-12-06 20:40:21 -0600

    pdf, base, loginAlam
    :)

---
+++ php/configbd.php
@@ -0,0 +1,7 @@
+<?php
+	$conn= mysql_connect("localhost","root","") or die('No se pudo conectar');
+	mysql_select_db("escomtitulos",$conn) or die (
+	"Error: No es posible establecer la conexión"
+	);
+	//mysqli_set_charset($conn, "utf8");
+?>
