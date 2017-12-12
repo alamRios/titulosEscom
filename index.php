7c2f8ccb47938b9f4b436ae1be027eeee18d30f2
Floranyelli Anaya <33078992+FloranyelliA@users.noreply.github.com>
2017-12-12 00:19:10 -0600

    index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 	  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
 	  <script type="text/javascript" src="js/materialize.min.js"></script>
-	  <script type="text/javascript" src="js/index.js"></script>
+	  <script type="text/javascript" src="js/index1.js"></script>
           <script type="text/javascript" src="js/sweetalert2.all.js"></script>
   </head>
   <body>
